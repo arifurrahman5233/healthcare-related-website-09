ca5997b4bc00f39c2a284afeb219499abb54d9e1
Arif <arifurrahman5233>
2021-10-19 23:34:20 +0600

    final

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyACBoGz6i5XvnXHWZ-wwDilFLoDVUaJgyw",
+    authDomain: "fir-health-care.firebaseapp.com",
+    projectId: "fir-health-care",
+    storageBucket: "fir-health-care.appspot.com",
+    messagingSenderId: "594680257212",
+    appId: "1:594680257212:web:2ed155a085c0facab04eb9"
+};
+
+export default firebaseConfig;
